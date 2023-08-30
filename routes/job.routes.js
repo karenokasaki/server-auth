ce8faeda1f1ace25697185725716871000e8afd5
karen okasaki <oks.karen@gmail.com>
2023-08-30 01:42:29 -0300

    adding unapply route

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -55,6 +55,22 @@ jobRouter.get("/:id_job", isAuth, async (req, res) => {
    }
 });
 
+jobRouter.get("/:id_job/public", async (req, res) => {
+   try {
+      const id_job = req.params.id_job;
+
+      const job = await JobModel.findById(id_job).populate({
+         path: "business",
+         select: "name email telefone description",
+      });
+
+      return res.status(200).json(job);
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+   }
+});
+
 //get ALL JOBS open
 jobRouter.get("/all/open", isAuth, async (req, res) => {
    try {
@@ -128,6 +144,31 @@ jobRouter.post("/apply/:id_job", isAuth, async (req, res) => {
       // adicionar o job na array de history_jobs do usuário
       await UserModel.findByIdAndUpdate(id_user, {
          $push: { history_jobs: id_job },
+      });
+
+      return res
+         .status(200)
+         .json({ message: "Você se candidatou para essa vaga de emprego!" });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+   }
+});
+
+//unapply
+jobRouter.post("/unapply/:id_job", isAuth, async (req, res) => {
+   try {
+      const id_job = req.params.id_job;
+      const id_user = req.auth._id;
+
+      // adicionar o id do user a array candidates
+      await JobModel.findByIdAndUpdate(id_job, {
+         $pull: { candidates: id_user },
+      });
+
+      // adicionar o job na array de history_jobs do usuário
+      await UserModel.findByIdAndUpdate(id_user, {
+         $pull: { history_jobs: id_job },
       });
 
       return res
